94a5a8e066c0265c025ae098bb6dd6fd3bc75473
Collin Lowing <collin.lowing@protonmail.com>
2022-03-23 20:41:03 -0500

    graphviz

--- draw.py
+++ draw.py
@@ -1,29 +1,39 @@
-import networkx as nx
-import matplotlib.pyplot as plt
+# import networkx as nx
+# import matplotlib.pyplot as plt
+#
+# G = nx.DiGraph()
+# G.add_node(0), G.add_node(1), G.add_node(2), G.add_node(3), G.add_node(4)
+# G.add_edge(0, 1), G.add_edge(1, 2), G.add_edge(0, 2), G.add_edge(1, 4), G.add_edge(1, 3), G.add_edge(3, 2), G.add_edge(
+#     3, 1), G.add_edge(4, 3), G.add_edge(0, 0, name="x")
+#
+# nx.draw(G, with_labels=True, font_weight='bold')
+# plt.show()
 
-G = nx.DiGraph()
-G.add_edges_from(
-    [('A', 'B'), ('A', 'C'), ('D', 'B'), ('E', 'C'), ('E', 'F'),
-     ('B', 'H'), ('B', 'G'), ('B', 'F'), ('C', 'G')])
+import graphviz
 
-val_map = {'A': 1.0,
-           'D': 0.5714285714285714,
-           'H': 0.0}
+f = graphviz.Digraph('finite_state_machine', filename='fsm.gv')
+f.attr(rankdir='LR', size='8,5')
 
-values = [val_map.get(node, 0.25) for node in G.nodes()]
+f.attr('node', shape='doublecircle')
+f.node('LR_0')
+f.node('LR_3')
+f.node('LR_4')
+f.node('LR_8')
 
-# Specify the edges you want here
-red_edges = [('A', 'C'), ('E', 'C')]
-edge_colours = ['black' if not edge in red_edges else 'red'
-                for edge in G.edges()]
-black_edges = [edge for edge in G.edges() if edge not in red_edges]
+f.attr('node', shape='circle')
+f.edge('LR_0', 'LR_2', label='SS(B)')
+f.edge('LR_0', 'LR_1', label='SS(S)')
+f.edge('LR_1', 'LR_3', label='S($end)')
+f.edge('LR_2', 'LR_6', label='SS(b)')
+f.edge('LR_2', 'LR_5', label='SS(a)')
+f.edge('LR_2', 'LR_4', label='S(A)')
+f.edge('LR_5', 'LR_7', label='S(b)')
+f.edge('LR_5', 'LR_5', label='S(a)')
+f.edge('LR_6', 'LR_6', label='S(b)')
+f.edge('LR_6', 'LR_5', label='S(a)')
+f.edge('LR_7', 'LR_8', label='S(b)')
+f.edge('LR_7', 'LR_5', label='S(a)')
+f.edge('LR_8', 'LR_6', label='S(b)')
+f.edge('LR_8', 'LR_5', label='S(a)')
 
-# Need to create a layout when doing
-# separate calls to draw nodes and edges
-pos = nx.spring_layout(G)
-nx.draw_networkx_nodes(G, pos, cmap=plt.get_cmap('jet'),
-                       node_color = values, node_size = 500)
-nx.draw_networkx_labels(G, pos)
-nx.draw_networkx_edges(G, pos, edgelist=red_edges, edge_color='r', arrows=True)
-nx.draw_networkx_edges(G, pos, edgelist=black_edges, arrows=False)
-plt.show()
+f.view()
